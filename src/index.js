2479def117127ed18da9f243f297eeea466ea00d
Gagan Manda <mngagan@gmail.com>
2020-10-20 14:44:46 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+const Index = () => {
+  return <div style = {{width : '500px', height : "500px"}}>Welcome to React --- namste -- jai balaya</div>;
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
